da4868b2adb32893381eaf6957bd7cf1fbeb9dbb
xust <xushitong@uniontech.com>
2024-11-28 17:30:35 +0800

    fix: Remove view mode settings when reset view mode
    When resetting view mode, remove the view mode settings from
    FileViewState instead of setting it to default value. This ensures the
    view mode settings are completely cleared.
    
    Log: Fix view mode reset behavior by removing settings instead of
    setting default value
    
    Bug: https://pms.uniontech.com/bug-view-286389.html
    Change-Id: Ib5a6c04bd4ce1a8d6a81b985f4574f02b50c5f98

--- src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
+++ src/plugins/filemanager/core/dfmplugin-workspace/views/fileview.cpp
@@ -985,9 +985,6 @@ void FileView::onDefaultViewModeChanged(int mode)
     if (newMode == Global::ViewMode::kTreeMode && !WorkspaceHelper::instance()->supportTreeView(rootUrl().scheme()))
         return;
 
-    if (newMode == d->currentViewMode)
-        return;
-
     Global::ViewMode oldMode = d->currentViewMode;
     loadViewState(rootUrl());
 
