10b0b7087c47985b0dd3a5e1cc0364e79d666e9d
Gong Heng <gongheng@uniontech.com>
2025-03-11 10:06:00 +0800

    fix: [file-size] Directory size display shows incorret values
    --The size of linkd files is being incorrectly added to parent directory's total size.
    
    Log: fix issue
    Bug: https://pms.uniontech.com/bug-view-307399.html
    
    Change-Id: I1a68c76cfc1fffeb238e9171c91123cbe2826933

--- src/plugins/common/core/dfmplugin-propertydialog/views/basicwidget.cpp
+++ src/plugins/common/core/dfmplugin-propertydialog/views/basicwidget.cpp
@@ -41,6 +41,7 @@ BasicWidget::BasicWidget(QWidget *parent)
 {
     initUI();
     fileCalculationUtils = new FileStatisticsJob;
+    fileCalculationUtils->setFileHints(FileStatisticsJob::FileHint::kNoFollowSymlink);
 }
 
 BasicWidget::~BasicWidget()
