1b4b4a11399b672b2e9496656f9456c4e203f5bb
xust <xushitong@uniontech.com>
2025-08-28 19:03:07 +0800

    Revert "fix(ddplugin-organizer): optimize frame management and desktop organization"
    This reverts commit 35f68f47cb08a9ac0a54becd5be00fcc90963220.
    
    Change-Id: Ia1a482ac15344ff78545a1a0ad25ef93bf50e6cc

--- src/plugins/desktop/ddplugin-organizer/framemanager.cpp
+++ src/plugins/desktop/ddplugin-organizer/framemanager.cpp
@@ -185,6 +185,8 @@ void FrameManagerPrivate::enableChanged(bool e)
         q->turnOn();
     } else {
         q->turnOff();
+        if (CfgPresenter->organizeOnTriggered())
+            CfgPresenter->saveNormalProfile({});   // feature
     }
 }
 
@@ -260,15 +262,6 @@ QWidget *FrameManagerPrivate::findView(QWidget *root) const
     return nullptr;
 }
 
-void FrameManagerPrivate::onOrganizered()
-{
-    if (organizer)
-        return;
-    q->onBuild();
-    for (const SurfacePointer &sur : surfaceWidgets.values())
-        sur->show();
-}
-
 FrameManager::FrameManager(QObject *parent)
     : QObject(parent), d(new FrameManagerPrivate(this))
 {
@@ -297,10 +290,9 @@ bool FrameManager::initialize()
     bool enable = CfgPresenter->isEnable();
     fmInfo() << "Organizer enable:" << enable;
     if (enable)
-        turnOn();   // builded by signal.
+        turnOn(false);   // builded by signal.
 
     connect(CfgPresenter, &ConfigPresenter::changeEnableState, d, &FrameManagerPrivate::enableChanged, Qt::QueuedConnection);
-    connect(CfgPresenter, &ConfigPresenter::reorganizeDesktop, d, &FrameManagerPrivate::onOrganizered, Qt::QueuedConnection);
     connect(CfgPresenter, &ConfigPresenter::changeEnableVisibilityState, d, &FrameManagerPrivate::enableVisibility, Qt::QueuedConnection);
     connect(CfgPresenter, &ConfigPresenter::changeHideAllKeySequence, d, &FrameManagerPrivate::saveHideAllSequence, Qt::QueuedConnection);
     connect(CfgPresenter, &ConfigPresenter::switchToNormalized, d, &FrameManagerPrivate::switchToNormalized, Qt::QueuedConnection);
@@ -341,7 +333,7 @@ void FrameManager::switchMode(OrganizerMode mode)
     d->organizer->initialize(d->model);
 }
 
-void FrameManager::turnOn()
+void FrameManager::turnOn(bool build)
 {
 #ifdef QT_DEBUG
     QDBusInterface ifs("com.deepin.dde.desktop",
@@ -364,6 +356,14 @@ void FrameManager::turnOn()
 
     d->model = new CollectionModel(this);
     d->model->setModelShell(d->canvas->fileInfoModel());
+
+    if (build) {
+        onBuild();
+
+        // show surface
+        for (const SurfacePointer &sur : d->surfaceWidgets.values())
+            sur->setVisible(true);
+    }
 }
 
 void FrameManager::turnOff()
