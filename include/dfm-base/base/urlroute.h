a179ff92d31a21f0ed40482006ba7513851d0587
liyigang <liyigang1@uniontech.com>
2025-11-12 16:04:51 +0800

    perf(urlroot):ABI compatibility issue modification
     - Add static variables for kSchemeInfos and kSchemeRealTree on version 5.0.58.25 and declare them obsolete
    
    Log: ABI compatibility issue modification
    Change-Id: Ibe0b376493f537661c7344a76f370ea38e33db22

--- include/dfm-base/base/urlroute.h
+++ include/dfm-base/base/urlroute.h
@@ -41,6 +41,8 @@ public:
 class UrlRoute
 {
 private:
+    [[deprecated("Abandoned")]]  static QHash<QString, SchemeNode> kSchemeInfos;   // info cache
+    [[deprecated("Abandoned")]]  static QMultiMap<int, QString> kSchemeRealTree;   // index cache
     static QHash<QString, SchemeNode> &schemeInfos();   // info cache
     static QMultiMap<int, QString> &schemeRealTree();   // index cache
 public:
