2d7dd534ee56aac2565b5b2ad15f5a841d65f3d4
gongheng <gongheng@uniontech.com>
2025-12-03 15:15:41 +0800

    Fix: Improve tag widget dynamic height and scrolling behavior
    - Add dynamic height calculation for tag crumb edit widget
    - Disable vertical scrollbar for better UI consistency
    - Remove fixed height constraints in tag widget
    - Connect document layout size changes to update widget height dynamically
    
    Log: fix bug
    Bug: https://pms.uniontech.com/bug-view-337133.html
    Change-Id: Ieac5eaf766c04a5b7c7059521f68a4f942d0873e

--- src/plugins/common/dfmplugin-tag/widgets/tagcrumbedit.cpp
+++ src/plugins/common/dfmplugin-tag/widgets/tagcrumbedit.cpp
@@ -4,6 +4,9 @@
 
 #include "tagcrumbedit.h"
 
+#include <cmath>
+#include <QAbstractTextDocumentLayout>
+
 DWIDGET_USE_NAMESPACE
 using namespace dfmplugin_tag;
 
@@ -13,6 +16,13 @@ TagCrumbEdit::TagCrumbEdit(QWidget *parent)
     auto doc = QTextEdit::document();
     doc->setDocumentMargin(doc->documentMargin() + 5);
     setViewportMargins(3, 3, 3, 3);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    edit = qobject_cast<QTextEdit *>(this);
+    if (edit) {
+        QAbstractTextDocumentLayout *layout = edit->document()->documentLayout();
+        connect(layout, &QAbstractTextDocumentLayout::documentSizeChanged, this, &TagCrumbEdit::updateHeight);
+    }
 }
 
 bool TagCrumbEdit::isEditing()
@@ -25,4 +35,12 @@ void TagCrumbEdit::mouseDoubleClickEvent(QMouseEvent *event)
     isEditByDoubleClick = true;
     DCrumbEdit::mouseDoubleClickEvent(event);
     isEditByDoubleClick = false;
+}
+
+void TagCrumbEdit::updateHeight()
+{
+    qreal docHeight = edit->document()->size().height();
+    QMargins margins = edit->contentsMargins();
+    int totalHeight = std::ceil(docHeight) + margins.top() + margins.bottom();
+    setFixedHeight(totalHeight);
 }
